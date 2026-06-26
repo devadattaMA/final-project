42711578c61eb41c164a9f7a0cfbd0f203f453cc
devadattaMA <61188450+devadattaMA@users.noreply.github.com>
2026-06-26 13:52:49 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Test database connection first - check terminal for result
+        DatabaseManager.testConnection();
+
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 LoginFrame loginFrame = new LoginFrame();
